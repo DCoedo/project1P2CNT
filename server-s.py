7ab2c387d82f722bc9cdb6e9e4eeee9802f47c39
DCoedo <98480357+DCoedo@users.noreply.github.com>
2022-02-18 13:54:04 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,4 +1,5 @@
 import socket
+import signal
 import sys
 import argparse
 
@@ -9,7 +10,7 @@ def readMsg(client, target):
     bytes_read = 0
     while connected:
         try:
-            msg = connected.recv(1024)
+            msg = client.recv(1024)
         except Exception:
             sys.stderr.write("ERROR:")
             connected = False
@@ -33,19 +34,23 @@ def handle_client():
     except Exception:
         sys.stderr.write("ERROR:")
         exit(1)
-        
-    server.listen(10)
+
     server.settimeout(10)
+    server.listen(10)
 
     while connected:
-        connection, connection_address = server.accept()
-        connection.send(bytes('accio\r\n', FORMAT))
-        confirm_1_bytes = readMsg(connection, b'confirm-accio\r\n')
-        connection.send(bytes('accio\r\n', FORMAT))
-        confirm_2_bytes = readMsg(connection, b'confirm-accio-again\r\n\r\n')
-        total_bytes = readMsg(connection, "")
-        total_bytes = total_bytes - confirm_1_bytes + confirm_2_bytes
-        connection.close()
-        print(total_bytes)
+        try:
+            connection, connection_address = server.accept()
+            connection.send(bytes('accio\r\n', FORMAT))
+            confirm_1_bytes = readMsg(connection, b'confirm-accio\r\n')
+            connection.send(bytes('accio\r\n', FORMAT))
+            confirm_2_bytes = readMsg(connection, b'confirm-accio-again\r\n\r\n')
+            total_bytes = readMsg(connection, b"")
+            total_bytes = total_bytes - confirm_1_bytes + confirm_2_bytes
+            connection.close()
+            print(total_bytes)
+        except Exception:
+            sys.stderr.write("ERROR")
+            exit(2)
 
 handle_client()
