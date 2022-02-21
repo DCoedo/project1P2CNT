89e516d71f5e34658479b19dea29e154e494b781
DCoedo <98480357+DCoedo@users.noreply.github.com>
2022-02-20 22:44:21 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -6,28 +6,26 @@ import argparse
 
 def readConfirm(client, target):
     connected = True
-    client.settimeout(10)
     msg = b""
     while connected:
         try:
             msg += client.recv(8)
         except socket.timeout:
-            sys.stderr.write("ERROR:")
+            sys.stderr.write("ERROR")
             break
         if msg == target:
-            connected = False
+            break
 
 
 def readMsg(client):
     connected = True
-    client.settimeout(10)
     bytes_read = 0
+    msg = b''
     while connected:
-        msg = b''
         try:
             msg = client.recv(8)
         except socket.timeout:
-            sys.stderr.write("ERROR:")
+            sys.stderr.write("ERROR")
             break
         if len(msg) == 0:
             break
@@ -43,16 +41,16 @@ def handle_client():
 
     connected = True
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
 
     try:
         server.bind(ADDR)
-    except Exception:
+    except Exception():
         sys.stderr.write("ERROR:")
         exit(1)
 
     server.settimeout(10)
     server.listen(10)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 2048)
     # print('Real bytes 1: ', len(b'confirm-accio\r\n'))
     # print('Real bytes 2: ', len(b'confirm-accio-again\r\n\r\n'))
     while connected:
@@ -60,6 +58,7 @@ def handle_client():
             signal.signal(signal.SIGINT, exit)
             signal.signal(signal.SIGTERM, exit)
             connection, connection_address = server.accept()
+            connection.settimeout(10)
             connection.send(bytes('accio\r\n', FORMAT))
             readConfirm(connection, b'confirm-accio\r\n')
             connection.send(bytes('accio\r\n', FORMAT))
@@ -70,5 +69,6 @@ def handle_client():
         except Exception():
             sys.stderr.write('ERROR')
             exit(2)
-            
+
+
 handle_client()
